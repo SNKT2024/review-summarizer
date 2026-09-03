12b9db2a00b83da6170ec15c8dda80ca7504fcf4
Sanket <sanketkumbharstu@gmail.com>
2026-09-03 10:36:05 +0530

    Initialized project with client and server setup

---
+++ packages/server/routes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { Request, Response } from "express";
+import { PrismaClient } from "./src/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { reviewController } from "./src/controllers/review.controller";
+const app = express;
+
+const router = app.Router();
+
+router.get("/api/products/:id/reviews", reviewController.getReviews);
+
+export default router;
